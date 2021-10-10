13706e29fa02e0958440c201b405c8506ac88f19
Jan Krajnc <jan.krajnc17@gmail.com>
2021-10-10 17:24:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+x = int(input("vnesite vrednost x: "))
+y = int(input("vnesite vrednost y: "))
+operation = input("izberite operacijo: +, -, *, / ")
+
+if operation == "+":
+    print(x + y)
+elif operation == "-":
+    print(x - y)
+elif operation == "*":
+    print(x * y)
+elif operation == "/":
+    print(x / y)
